880c28ac13160b4b83b693431754741c647dd71a
Vinicius <vinicius.alexandre68@gmail.com>
2022-09-06 20:22:10 -0300

    Add 'ci' alias to Mix

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule BilletManager.MixProject do
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [ci: :test]
     ]
   end
 
@@ -64,7 +65,8 @@ defmodule BilletManager.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      ci: ["format --check-formatted", "credo --strict", "test"]
     ]
   end
 end
