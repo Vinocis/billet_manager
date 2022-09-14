c14746cb6ca9fab2c11533e6c551d8cb8aeb924e
Vinicius <vinicius.alexandre68@gmail.com>
2022-09-13 22:16:25 -0300

    Add GraphQL query config

--- lib/billet_manager_web/schemas/customer/resolver.ex
+++ lib/billet_manager_web/schemas/customer/resolver.ex
@@ -1,13 +1,9 @@
 defmodule BilletManagerWeb.Schemas.Customer.Resolver do
   @moduledoc false
 
-  alias BilletManager.InstallmentsBasis.Models.Customer
   alias BilletManager.InstallmentsBasis.Services.CreateCustomer
   alias BilletManager.InstallmentsBasis.Services.GetCustomers
   alias BilletManager.InstallmentsBasis.Services.UpdateCustomer
-
-  @type changeset :: Ecto.Changeset.t()
-  @type customer :: Customer.t()
 
   def get_customers(_parent, _params, _context) do
     GetCustomers.process()
@@ -17,9 +13,7 @@ defmodule BilletManagerWeb.Schemas.Customer.Resolver do
     CreateCustomer.process(input)
   end
 
-  def update_customer(_parent, %{cpf: cpf, input: input}, _context) do
-    input
-    |> Map.put(:cpf, cpf)
-    |> UpdateCustomer.process()
+  def update_customer(_parent, params, _context) do
+    UpdateCustomer.process(params)
   end
 end
--- test/billet_manager_web/schemas/customer/query_test.exs
+++ test/billet_manager_web/schemas/customer/query_test.exs
@@ -1,8 +1,6 @@
 defmodule BilletManagerWeb.Schemas.Customer.QueryTest do
   use BilletManagerWeb.ConnCase
 
-  alias BilletManager.InstallmentsBasis.Services.CreateCustomer
-
   @moduletag :integration
 
   @query """
@@ -14,21 +12,60 @@ defmodule BilletManagerWeb.Schemas.Customer.QueryTest do
   }
   """
 
+  @query_with_billets """
+  query{
+    customers{
+      name
+      cpf
+      billets {
+        code
+        expireOn
+        status
+        value
+      }
+    }
+  }
+  """
+
   describe "Customers query" do
     test "list all customers", %{conn: conn} do
-      attrs = %{
-        cpf: "111.444.777-35",
-        name: "Jhon Doe"
-      }
-
-      CreateCustomer.process(attrs)
+      insert(:customer)
 
       response = post(conn, "/api/v1", %{"query" => @query})
 
-      assert [data | _tail] = json_response(response, 200)["data"]["customers"]
+      assert [data] = json_response(response, 200)["data"]["customers"]
 
-      assert data["cpf"] == attrs.cpf
-      assert data["name"] == attrs.name
+      assert data["cpf"] == "111.444.777-35"
+      assert data["name"] == "Jhon Doe"
+    end
+
+    test "list all customers with billets", %{conn: conn} do
+      customer = insert(:customer)
+
+      insert(:billet, code: "abc123", customer: customer)
+      insert(:billet, code: "def456", customer: customer)
+
+      response = post(conn, "/api/v1", %{"query" => @query_with_billets})
+
+      assert [data1] = json_response(response, 200)["data"]["customers"]
+
+      assert data1["cpf"] == "111.444.777-35"
+      assert data1["name"] == "Jhon Doe"
+
+      assert data1["billets"] == [
+               %{
+                 "code" => "abc123",
+                 "expireOn" => "2023-09-02",
+                 "status" => "OPENED",
+                 "value" => 10_000
+               },
+               %{
+                 "code" => "def456",
+                 "expireOn" => "2023-09-02",
+                 "status" => "OPENED",
+                 "value" => 10_000
+               }
+             ]
     end
   end
 end
