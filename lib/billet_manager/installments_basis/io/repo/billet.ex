0fb2050dc588aa2647dfe4521720af4e8e837050
Vinicius <vinicius.alexandre68@gmail.com>
2022-09-19 18:35:20 -0300

    Add new functions to billet repo

--- lib/billet_manager/installments_basis/io/repo/billet.ex
+++ lib/billet_manager/installments_basis/io/repo/billet.ex
@@ -18,6 +18,40 @@ defmodule BilletManager.InstallmentsBasis.IO.Repo.Billet do
     |> Repo.insert()
   end
 
+  @impl true
+  @doc """
+  Update a billet
+  """
+  def update(billet, attrs) do
+    billet
+    |> Billet.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Fetch billets by params
+  """
+  @impl true
+  def fetch_by(params) do
+    fetch_by(Billet, params)
+  end
+
+  @doc """
+  Fetch all billets linked to the customer_id 
+  passed as a parameter
+
+  ##Example
+    iex> BilletManager.InstallmentsBasis.IO.Repo.Billet.get_billets_by_customer_id(1)
+    [
+      %{
+        code: "3codfe214f",
+        expire_on: ~D[2022-10-03],
+        paid_value: nil
+        status: :opened,
+        value: %Money{amount: 1111, currency: :BRL}
+      }
+    ]
+  """
   def get_billets_by_customer_id(customer_id) do
     BilletQueries.root_query()
     |> BilletQueries.inner_join_by_customer()
